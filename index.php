41040a6df857f24a741b8d18b0f561e7253f8bf5
Fika Febrika <fikafebrika02@gmail.com>
2022-04-01 10:36:34 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, obcaecati quae harum odit neque totam commodi dolorum consectetur? Expedita atque aliquam est. Libero dolore nihil sit est adipisci? Nobis, error?</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
